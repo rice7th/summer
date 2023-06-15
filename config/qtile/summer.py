ae406b9445e9f6e1639e35ddf1b8672b88805979
tsukki9696 <viniciusdesouzagvitorino@gmail.com>
2023-06-15 20:48:51 -0300

    added qtile colorscheme

---
+++ config/qtile/summer.py
@@ -0,0 +1,21 @@
+#Summer
+def summer():
+    colors = [['#1D1D1D', '#1D1D1D'],      #background (black) [0]
+                ['#626861', '#626861'],    #ebony [1]
+                ['#D8E2E1', '#D8E2E1'],    #foreground (white) [2]
+                ['#CF3746', '#CF3746'],    #red [3]
+                ['#DF7C2C', '#DF7C2C'],    #orange [4]
+                ['#ECBD10', '#ECBD10'],    #saffron [5]
+                ['#7CBD27', '#7CBD27'],    #green [6]
+                ['#41A36F', '#41A36F'],    #jade [7]
+                ['#32B5C7', '#32B5C7'],    #pacific blue [8]
+                ['#277AB6', '#277AB6'],    #star command blue [9]
+                ['#AD4ED2', '#AD4ED2'],    #orchid [10]
+                ['#776550', '#776550']]    #umber [11]
+
+    backgroundColor = '#1D1D1D'
+    foregroundColor = '#D8E2E1'
+    workspaceColor = '#CF3746' #can be any color you want
+    foregroundColorTwo = '#626861'
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
+
